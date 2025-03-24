14fcaed0bf7466c35e709eb1edc40a0b64af24de
loyal <102851403+loyaltly-cn@users.noreply.github.com>
2025-03-25 00:30:11 +0800

    update

---
+++ mp/src/pages/my/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-black">
+    my
+  </h1>
+</template>
+
+<route type="home" lang="json">
+    {
+      "style": {
+        "navigationBarTitleText": "my"
+      }
+    }
+    </route>
