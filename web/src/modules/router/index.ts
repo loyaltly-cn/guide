0ca0b27c95900dda34aaa8cd3ebfa08c7a8d0371
loyal <loyaltly.cn@gmail.com>
2025-03-26 22:07:10 +0800

    update

--- web/src/modules/router/index.ts
+++ web/src/modules/router/index.ts
@@ -3,6 +3,7 @@ import {createRouter, createWebHistory, type RouteRecordRaw} from "vue-router";
 import test from '@/pages/test/index.vue'
 import _404 from "@pages/_404/index.tsx";
 import Home from '@pages/home/index.vue'
+import Login from '@pages/login/index.vue'
 
 const list:Array<RouteRecordRaw> = [{
     path:'/',
@@ -10,6 +11,9 @@ const list:Array<RouteRecordRaw> = [{
 },{
     path:'/home',
     component:Home,
+},{
+    path:'/login',
+    component:Login,
 },{
     path:'/test',
     component:test,
